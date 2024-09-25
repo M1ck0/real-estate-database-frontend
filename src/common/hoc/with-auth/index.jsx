d7def1383e41aedcbc63a52a5146f67e980c2b44
miletadulovic <dulovicmileta1@gmail.com>
2024-09-26 01:52:09 +0200

    with auth

--- src/common/hoc/with-auth/index.jsx
+++ src/common/hoc/with-auth/index.jsx
@@ -1,6 +1,6 @@
 import { supabase } from "client";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const WithAuth = ({ children }) => {
   const [auth, setAuth] = useState(true);
@@ -9,6 +9,8 @@ const WithAuth = ({ children }) => {
 
   const getUser = async () => {
     const { data } = await supabase.auth.getSession();
+
+    console.log(data);
 
     if (data?.session) {
       setAuth(true);
@@ -22,7 +24,9 @@ const WithAuth = ({ children }) => {
     getUser();
   }, []);
 
-  return auth ? children : <></>;
+  console.log("auth: ", auth);
+
+  return <Outlet />;
 };
 
 export default WithAuth;
